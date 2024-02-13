559808dd68153e32e1b8bebad7ac468726500ffa
hahahhahahsgsghsgsgs <159803292+hahahhahahsgsghsgsgs@users.noreply.github.com>
2024-02-13 16:59:07 +0700

    Update gcs.py

--- ubot/core/plugins/gcs.py
+++ ubot/core/plugins/gcs.py
@@ -64,12 +64,15 @@ broadcast_running = False
 
 
 async def broadcast_group_cmd(client, message):
+    global broadcast_running
 
-    msg = await message.reply("Sedang Jovio proses kak....", quote=True)
+    msg = await message.reply("Processing...", quote=True)
 
     send = get_message(message)
     if not send:
         return await msg.edit("Silakan balas ke pesan atau berikan pesan.")
+
+    broadcast_running = True
 
     chats = await get_broadcast_id(client, "group")
     blacklist = await get_chat(client.me.id)
@@ -78,76 +81,49 @@ async def broadcast_group_cmd(client, message):
     failed = 0
 
     for chat_id in chats:
-        if chat_id in blacklist:
-            continue
-        elif chat_id in BLACKLIST_CHAT:
-            continue
-        try:
-            if message.reply_to_message:
-                await send.copy(chat_id)
-            else:
-                await client.send_message(chat_id, send)
-            await asyncio.sleep(0.2)
-            done += 1
-        except FloodWait:
-            continue
-        except SlowmodeWait:
-            continue
-        except Exception:
-            continue
-        except BaseException:
-            failed += 1
-    await msg.edit(
-            f"**pesan broadcast Anda terkirim ke {done} grup. gagal: {failed}**."
+        if not broadcast_running:
+            break
+
+        if chat_id not in blacklist and chat_id not in BLACKLIST_CHAT:
+            try:
+                if message.reply_to_message:
+                    await send.copy(chat_id)
+                else:
+                    await client.send_message(chat_id, send)
+                done += 1
+                # await asyncio.sleep(2)
+            # except FloodWait as e:
+            # await asyncio.sleep(e.value)
+            # except SlowmodeWait as e:
+            # await asyncio.sleep(e.value)
+
+            except Exception:
+                failed += 1
+
+    broadcast_running = False
+
+    if done > 0:
+        await msg.edit(
+            f"**Successfully Sent Message To `{done}` Groups chat. Failed: `{failed}`**."
         )
-        
-        
-async def continuous_broadcast(client, message):
-    tasks = [broadcast_group_cmd(client, message) for _ in range(5)]  # Membuat 5 tugas broadcast
-    await asyncio.gather(*tasks)
-    await asyncio.sleep(0.2)
+    else:
+        await msg.edit(f"**Pesan Broadcast Berhasil Dibatalkan**.")
 
-    msg = await message.reply("Sedang memproses, mohon bersabar...")
 
-    send = get_message(message)
-    if not send:
-        return await msg.edit("Mohon balas sesuatu atau ketik sesuatu")
+async def cancel_broadcast(client, message):
+    global broadcast_running
 
-    chats = await get_broadcast_id(client, "group")
-    blacklist = await get_chat(client.me.id)
+    if not broadcast_running:
+        return await message.reply_text(
+            "<code>Tidak ada pengiriman pesan broadcast yang sedang berlangsung.</code>"
+        )
 
-    done = 0
-    for chat_id in chats:
-        if chat_id in blacklist or chat_id in BLACKLIST_CHAT:
-            continue
+    broadcast_running = False
+    await message.delete()
 
-        try:
-            if message.reply_to_message:
-                await send.copy(chat_id)
-            else:
-                await client.send_message(chat_id, send)
-            done += 1
-            await asyncio.sleep(0.2)  # Tambahkan penundaan kecil setelah setiap pesan
-
-        except FloodWait as e:
-            # Handle SlowmodeWait separately
-            if e.x >= 1:
-                await asyncio.sleep(e.x + 1)  # Tambahkan waktu tambahan untuk menghindari kemungkinan SlowmodeWait
-            else:
-                await asyncio.sleep(1)  # Jika tidak ada informasi waktu yang diberikan
-            if message.reply_to_message:
-                await send.copy(chat_id)
-            else:
-                await client.send_message(chat_id, send)
-            done += 1
-        except Exception:
-            pass
-
-    return await msg.edit(f"<b>pesan broadcast Anda terkirim ke {done} grup</b>")
-        
 
 async def broadcast_users_cmd(client, message):
-    msg = await message.reply("sabar pakde kirim surat cintamu....")
+    msg = await message.reply("Processing...")
     blacklist = await get_chat(client.me.id)
     done = 0
     async for dialog in client.get_dialogs(limit=None):
@@ -156,7 +132,7 @@ async def broadcast_users_cmd(client, message):
                 send = message.reply_to_message
             else:
                 if len(message.command) < 2:
-                    return await msg.edit("Silakan balas ke pesan atau berikan pesan")
+                    return await msg.edit("Silakan balas ke pesan atau berikan pesan.")
                 else:
                     send = message.text.split(None, 1)[1]
             chat_id = dialog.chat.id
@@ -170,7 +146,7 @@ async def broadcast_users_cmd(client, message):
                 except Exception:
                     pass
 
-    await msg.edit(f"**🐒 berhasil kirim pesan ke {done} orang gabut.**")
+    await msg.edit(f"**Successfully Sent Message To `{done}` Groups chat**")
 
 
 async def send_msg_cmd(client, message):
@@ -187,7 +163,7 @@ async def send_msg_cmd(client, message):
                 await client.send_inline_bot_result(
                     chat_id, x.query_id, x.results[0].id
                 )
-                tm = await message.reply(f"✅Pesan berhasil dikirim ke {chat_id}")
+                tm = await message.reply(f"✅ Pesan berhasil dikirim ke {chat_id}")
                 await message.delete()
                 await tm.delete()
             except Exception as error:
