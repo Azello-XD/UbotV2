dbde566afe13d7bb2adb3f14522eccd2d2a897df
hahahhahahsgsghsgsgs <159803292+hahahhahahsgsghsgsgs@users.noreply.github.com>
2024-02-14 22:08:23 +0700

    Update absen.py

--- ubot/modules/absen.py
+++ ubot/modules/absen.py
@@ -5,7 +5,12 @@ from ubot import *
 async def _(client: Client, message: Message):
     msg = await message.reply("<b>apa jink?</b>")
 
+@PY.UBOT("buy")
+async def _(client: Client, message: Message):
+    msg = await message.reply("<b>di joviooo laa</b>")
+    
 
 @PY.UBOT("absen")
 async def _(client: Client, message: Message):
     msg = await message.reply("<b>Hadir kontoI</b>")
+    
