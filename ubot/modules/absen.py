1a141d0a004f527f30c9ce300e4dde6b23aaa8a4
voxydX <160240185+voxydX@users.noreply.github.com>
2024-02-19 20:12:51 +0700

    Update absen.py

--- ubot/modules/absen.py
+++ ubot/modules/absen.py
@@ -1,31 +1,11 @@
 from ubot import *
 
 
-@PY.UBOT("tian")
-async def _(client: Client, message: Message):
-    msg = await message.reply("<b>kontol</b>")
-
 @PY.UBOT("hai")
 async def _(client: Client, message: Message):
     msg = await message.reply("<b>hadir capt🔥🙌💯</b>")
     
 
-@PY.UBOT("absen")
-async def _(client: Client, message: Message):
-    msg = await message.reply("<b>hadir maniszz😋😋</b>")
-    
-@PY.UBOT("p")
-async def _(client: Client, message: Message):
-    msg = await message.reply("<b>salam memek🖕</b>")
-
 @PY.UBOT("dana")
 async def _(client: Client, message: Message):
     msg = await message.reply("<b>089525340368 \n Aria Putra Pratama</b>")
-    
-@PY.UBOT("q")
-async def _(client: Client, message: Message):
-    msg = await message.reply("<b>bot gacor pc jopii</b>")
-
-@PY.UBOT("aria")
-async def _(client: Client, message: Message):
-    msg = await message.reply("<b>apa ganteng😍</b>")
