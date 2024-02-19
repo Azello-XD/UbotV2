9710e2f31b3d14cdbe7c20111d8a3573707b072d
voxydX <160240185+voxydX@users.noreply.github.com>
2024-02-20 00:00:16 +0700

    Update gcs.py

--- ubot/core/plugins/gcs.py
+++ ubot/core/plugins/gcs.py
@@ -65,8 +65,7 @@ broadcast_running = False
 
 async def broadcast_group_cmd(client, message):
 
-    msg = await message.reply("sebentar, 
-sedang jovio proses kak...", quote=True)
+    msg = await message.reply("Tunggu, sedang jovio proses kak...", quote=True)
 
     send = get_message(message)
     if not send:
