a7834b6e76554e43e0ab6dafb12be43db56b7238
hahahhahahsgsghsgsgs <159803292+hahahhahahsgsghsgsgs@users.noreply.github.com>
2024-02-22 15:07:45 +0700

    Update str.py

--- ubot/core/plugins/str.py
+++ ubot/core/plugins/str.py
@@ -62,8 +62,8 @@ async def ping_cmd(client, message):
     if gua == True:
       _ping = f"""
 **<emoji id={cos_ping}>{cos_ping2}</emoji> Pong !!**
-**<emoji id={cos_pong}>{cos_pong2}</emoji>`{str(delta_ping).replace('.', ',')}ms`**
- **<emoji id=6035179905652626049>👁</emoji><b>Uptime :</b> <code>{uptime}</code>**"""
+**<emoji id={cos_pong}>{cos_pong2}</emoji> `{str(delta_ping).replace('.', ',')}ms`**
+**<emoji id=6035179905652626049>👁</emoji><b> Uptime :</b> <code>{uptime}</code>**"""
     elif gua == False:
       _ping = f"""
 **{cos_ping2} Pong !!**
